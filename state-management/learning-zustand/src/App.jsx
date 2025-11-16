5d565d24bca66814ff38f6ea514eda7ed85758d2
Karan Singh <devkaran.codes@gmail.com>
2025-11-16 21:32:12 +0530

    using the hooks to fetch, read & update data

--- state-management/learning-zustand/src/App.jsx
+++ state-management/learning-zustand/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import CourseForm from "./components/CourseForm";
+import CourseList from "./components/CourseList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="main-container">
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}>
+        <h1
+          style={{
+            marginBottom: "2rem",
+            fontSize: "2rem",
+          }}>
+          My Course List
+        </h1>
+        <CourseForm />
+        <CourseList />{" "}
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ state-management/learning-zustand/src/components/CourseForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import useCourseStore from "../app/courseStore";
+
+const CourseForm = () => {
+  const addCourse = useCourseStore((state) => state.addCourse);
+  const [courseTitle, setCourseTitle] = useState("");
+
+  const handleSubmit = () => {
+    if (!courseTitle) return alert("Please add a course title");
+    addCourse({
+      id: Math.ceil(Math.random() * 1000000),
+      title: courseTitle,
+      completed: false,
+    });
+    setCourseTitle("");
+  };
+  return (
+    <div className="form-container">
+      <input
+        value={courseTitle}
+        onChange={(e) => setCourseTitle(e.target.value)}
+        type="text"
+        className="form-input"
+        placeholder="Course title eg. Zustand series"
+      />
+      <button onClick={handleSubmit} className="form-submit-btn">
+        Add Course
+      </button>
+    </div>
+  );
+};
+
+export default CourseForm;
