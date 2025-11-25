b2b2f9191ae60a4d2b0ae933bb1ec36e52724983
Karan Singh <devkaran.codes@gmail.com>
2025-11-25 23:42:20 +0530

    fix

--- re-learn Postgres/Aggregation-fxns.sql
+++ re-learn Postgres/Aggregation-fxns.sql
@@ -3,5 +3,8 @@ select count(product_id) as Total_Products from products;
 select sum(price) as total_price from products;
 
 select sum(price) from products where category in ('Footwear', 'Furniture');
+select round(avg(price),2) from products;
+select min(price) from products;
+select max(price) from products;
 
 select * from products;
